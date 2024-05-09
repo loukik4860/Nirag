57369274335d9a2071b44dafe4904a7784e52bcc
loukik4860 <loukikhajare16@gmail.com>
2024-05-09 19:33:35 +0530

    added

---
+++ SchoolProject/SchoolApp/signals.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import *
+from django.utils import timezone
+
+
+@receiver(post_save, sender=SubjectModel)
+def calculate_percentage_of_student(sender, instance, created, **kwargs):
+    if created:
+        student = instance.child
+        marks = SubjectModel.objects.filter(child=student)
+        total_marks = sum([getattr(instance, field) for field in ['english', 'mathematics', 'chemistry', 'biology', 'history', 'geography', 'politics']])
+        percentage = round((total_marks / 700) * 100, 2)  # Round to 2 decimal places
+
+        historical_percentage, _ = historicalData.objects.update_or_create(
+            child=student,
+            defaults={
+                'year': timezone.now(),
+                'percentage': percentage
+            }
+        )
